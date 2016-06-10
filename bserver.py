643288e0ca27b8001f54d0a9462ce6753ec3014d
sun tianchen <s3130103354@gmail.com>
2016-06-10 12:01:11 +0800

    basic frame ok

---
+++ bserver.py
@@ -0,0 +1,43 @@
+import time
+import BaseHTTPServer
+import urlparse
+import sys
+
+HOST_NAME = '127.0.0.1' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 7010 # Maybe set this to 9000.
+
+with open('index.html', 'r') as myfile:
+    index = myfile.read().replace('\n', '')
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        gv = s.path.split('?')
+        print gv
+        if (len(gv) > 1):
+            print "gv len=" + str(len(gv))
+            gvd = urlparse.parse_qs(gv[1])
+            if 'uid' in gvd:
+                print gvd['uid'][0]
+        s.wfile.write(index)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        HOST_NAME = sys.argv[1]
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
---
+++ qrdecode.py
@@ -0,0 +1,103 @@
+'''
+Created on 2016-06-09
+'''
+
+from threading import Thread
+from threading import Event
+from Queue import Queue
+
+import subprocess
+import signal
+import time
+
+import zbar
+import Image
+import cv2
+
+class qrdecode(object):
+	def __init__(self):
+		pass
+
+	def decode(self,filename):
+		print "decode, filename is : " + filename
+		scanner = zbar.ImageScanner()
+		# configure the reader
+		scanner.parse_config('enable')
+		# obtain image data
+		pil = Image.open(filename).convert('L')
+		width, height = pil.size
+		raw = pil.tostring()
+		# wrap image data
+		image = zbar.Image(width, height, 'Y800', raw)
+		# scan the image for barcodes
+		scanner.scan(image)
+		# extract results
+		for symbol in image:
+			# do something useful with results
+			print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+			del(image)
+			return symbol.data
+		# clean up
+		del(image)
+		return False
+
+def alarm_handler(signum, frame):
+	raise Exception
+
+signal.signal(signal.SIGALRM, alarm_handler)
+
+class camera_task(Thread):
+	def __init__(self, queue, index=0, device = '1', filen="./qrcode.jpeg"):
+		Thread.__init__(self)
+		self.filename = filen
+		self.qr = qrdecode()
+		self.c = ['fswebcam', '-d' ,'/dev/video' + str(device),'--save',self.filename,'-S','2']
+		self.queue = queue
+		self._stop = Event()
+	'''
+	http://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread-in-python
+	'''
+	def stop(self):
+ 		self._stop.set()
+
+	def stopped(self):
+		return self._stop.isSet()
+
+	def run(self):
+		while True:
+			if self.stopped():
+				return
+			print "----camera----"
+			try:
+				process = subprocess.Popen(self.c)
+				process.communicate()
+				print "----camera done----"
+			except Exception:
+				continue
+			print "----qrdecode----"
+			try:
+				res = self.qr.decode(self.filename)
+				if res != False:
+					self.queue.put(res)
+					return
+			except Exception:
+				continue
+			time.sleep(1)
+
+def main():
+	while  True:
+		queue = Queue()
+		camt0 = camera_task(queue, 0, 0, 'len.jpeg')
+		camt0.daemon = True
+		camt = camera_task(queue, 0, 1)
+		camt.daemon = True
+		camt0.start()
+		camt.start()
+		s = queue.get()
+		print s
+		camt.stop()
+		camt0.stop()
+		raw_input()
+
+if __name__ == '__main__':
+	main()
